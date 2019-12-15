950f5b27df4e9570c1c2aee3c540e63a7cc75235
ahmad21-meet <57598476+ahmad21-meet@users.noreply.github.com>
2019-12-15 12:14:32 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -2,10 +2,10 @@ asus=[]
 users=[]
 posts=[]
 class  post ():
-	def __init__(self, user, content,date):
+	def __init__(self, user, content):
 		self.user=user
 		self.content=content
-		self.date=date
+	
 	
 class user(object):
 	def __init__(self,name,email,password):
@@ -17,32 +17,31 @@ class user(object):
 		asus.append(self.name)
 	def add_freind(self,email):
 		self.freind_list.append(email)
-		print(self.name+"email has add"+email+ "from  as freind_list")
+		print(self.name+"email has add."+ email + " from  as freind_list")
 	def remove_freind(self,email):
+		
 		self.freind_list.pop(email)
 		print(self.name+" has remove"+email+"from as a freind_list")
 	def add_posts(self,text):
-		posts.append(post(self.posts,text,"date"))
+		posts.append(post(self.posts,text))
 		print(self.name+"has posted",text)
-
 	def get_userInfo(self):
 		print("___________________")
 		print("Name:"+self.name)
 		print("Email:"+self.email)
 		print("Password:"+self.password)
-		print("Freind:"+str(self.freind_list))
-		
+		print("Freind:"+str(self.freind_list))	
 		print("___________________")
-user1=user("lolo","ahmad@mail.com","pass1234")
-user2=user("ames","ahmos@gmail.com","pass123456")
-user1.add_freind("mosa")
-user2.add_freind("meme")
-user2.add_posts("tetot")
-user2.add_posts("hi guys")
+user1=user("Ahmad "  ,  " Ahmad@mail.com"," i dont want to tell you my pass hhhhhhhh")
+user2=user("fadi " , "fadi@gmail.com","123456a")
+user1.add_freind(" sadeq")
+user2.add_freind(" osama ")
+
+user2.add_posts("  i am nerd:it mean i study all the time")
+user1.add_posts("sadeq ,i love you,mustafa you too i love you, jenefir you too i love  you,and mosa ,and ahmed ,i love you all ")
 user1.get_userInfo()
 user2.get_userInfo()
-
+user1.add_freind("  samer  ")
 print(asus)
-print(users)
 
 
